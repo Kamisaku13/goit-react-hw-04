7f4209655a55bd8af13e47b888980b2ca33d4f28
Kamisaku13 <147526361+Kamisaku13@users.noreply.github.com>
2024-05-26 11:03:44 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import { fetchPhotosByQuery } from './gallery-api';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import { Toaster } from 'react-hot-toast';
+import ImageModal from './components/ImageModal/ImageModal';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [totalPages, setTotalPages] = useState(1);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalImageSrc, setModalImageSrc] = useState('');
+  const [modalImageAlt, setModalImageAlt] = useState('');
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    async function fetchPhotos() {
+      setError(null);
+      setLoading(true);
+      try {
+        const { results, total_pages } = await fetchPhotosByQuery(
+          searchQuery,
+          page
+        );
+        if (total_pages === 0) {
+          throw new Error(
+            'Sorry, there are no images matching your search query'
+          );
+        }
+        setTotalPages(total_pages);
+        setPhotos(prevPhotos => {
+          return [...prevPhotos, ...results];
+        });
+      } catch (error) {
+        setError(`${error.message}. Please try again!`);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchPhotos();
+  }, [page, searchQuery]);
+
+  const handleSearch = query => {
+    setPhotos([]);
+    setSearchQuery(query);
+    setPage(1);
+    setTotalPages(1);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  // =============modal==============
+
+  function openModal(imgUrl, alt) {
+    setIsOpen(true);
+    setModalImageSrc(imgUrl);
+    setModalImageAlt(alt);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+    setModalImageSrc('');
+    setModalImageAlt('');
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div>
+      <SearchBar onSubmit={handleSearch} />
+      {photos.length > 0 && <ImageGallery items={photos} onClick={openModal} />}
+      {loading && <Loader />}
+      {error && !loading && <ErrorMessage>{error}</ErrorMessage>}
+      {page === totalPages && photos.length > 0 && (
+        <p
+          style={{
+            fontSize: 'medium',
+            marginBottom: 12,
+            textAlign: 'center',
+          }}
+        >
+          We`re sorry, there are no more images to load!
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      )}
+      {page < totalPages && !loading && photos.length > 0 && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      <ImageModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        modalImageUrl={modalImageSrc}
+        modalImageAlt={modalImageAlt}
+      />
+      <Toaster />
+    </div>
+  );
+};
 
-export default App
+export default App;
